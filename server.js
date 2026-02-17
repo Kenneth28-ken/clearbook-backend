eed4d931c0f4659aa71738f0332e178490a706e4
Kenneth <perfectmaney200@gmail.com>
2026-02-17 13:18:37 +0100

    Enable CORS

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/trigger", async (req, res) => {
